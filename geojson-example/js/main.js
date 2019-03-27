e1907424c3dc075a322af2847aa75e9bfaa246bc
dlousa92 <dlousa92@gmail.com>
2019-03-27 14:39:15 -0400

    add data types in geojson

--- geojson-example/js/main.js
+++ geojson-example/js/main.js
@@ -1,4 +1,4 @@
-const mymap = L.map('mapid').setView([39.725, -104.99], 13)
+const mymap = L.map('mapid').setView([39.743, -104.99], 13)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,3 +6,31 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZGxvdXNhOTIiLCJhIjoiY2p0cmQyMTVkMG90NjN6bzNpY2ppdW0zeSJ9.bIlClbOREYuQ2H0br8cC8A'
 }).addTo(mymap)
+
+// geoJSON data types
+const geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+}
+
+const geojsonLineString = [{
+    "type": "LineString",
+    "coordinates": [[-105, 39.75], [-105.10, 39.8], [-105, 39.81]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 39.74], [-105.05, 39.702], [-105.05, 39.65]]
+}]
+
+const myLayer = L.geoJSON().addTo(mymap)
+const secondLayer = L.geoJSON().addTo(mymap)
+myLayer.addData(geojsonFeature)
+secondLayer.addData(geojsonLineString)
+console.log(secondLayer)
