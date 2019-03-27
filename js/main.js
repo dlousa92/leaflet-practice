e1f0e426422472fda4c87bfe1d318a090ea55204
dlousa92 <dlousa92@gmail.com>
2019-03-27 11:44:10 -0400

    add marker, circle and polygon examples

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13)
+const mymap = L.map('mapid').setView([51.505, -0.09], 13)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZGxvdXNhOTIiLCJhIjoiY2p0cmQyMTVkMG90NjN6bzNpY2ppdW0zeSJ9.bIlClbOREYuQ2H0br8cC8A'
-}).addTo(mymap);
+}).addTo(mymap)
+
+
+// Markers, Circles and Polygons
+const marker = L.marker([51.5, -0.09]).addTo(mymap)
+
+const circle = L.circle([51.508, -0.11], {
+    color: 'blue',
+    fillColor: '#06a7cc',
+    radius: 300
+}).addTo(mymap)
+
+const polygon = L.polygon([
+    [51.525, -0.15],
+    [51.503, -0.06],
+    [51.52, -0.023],
+]).addTo(mymap)
