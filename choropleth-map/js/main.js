28d36280c516e51c6309ab9bb2820d04dc834f88
dlousa92 <dlousa92@gmail.com>
2019-03-29 12:54:29 -0400

    add data to map

--- choropleth-map/js/main.js
+++ choropleth-map/js/main.js
@@ -8,3 +8,5 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
     id: 'mapbox.light',
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>' 
 }).addTo(map)
+
+L.geoJson(statesData).addTo(map)
