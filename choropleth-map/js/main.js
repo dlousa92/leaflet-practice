0f1ca211e695b11ffb7681e9e0d90065561259d1
dlousa92 <dlousa92@gmail.com>
2019-03-29 12:52:45 -0400

    add map layer

--- choropleth-map/js/main.js
+++ choropleth-map/js/main.js
@@ -1,1 +1,10 @@
 console.log(statesData)
+
+// Mapbox Setup
+const mapboxAccessToken = 'pk.eyJ1IjoiZGxvdXNhOTIiLCJhIjoiY2p0cmQyMTVkMG90NjN6bzNpY2ppdW0zeSJ9.bIlClbOREYuQ2H0br8cC8A'
+const map = L.map('mapid').setView([37.8, -96], 4)
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+    id: 'mapbox.light',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>' 
+}).addTo(map)
