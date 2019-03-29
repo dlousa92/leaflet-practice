ba457fe5a674ac2c74974b32b1ac6d68ec87f993
dlousa92 <dlousa92@gmail.com>
2019-03-29 13:07:41 -0400

    write function to determine color

--- choropleth-map/js/main.js
+++ choropleth-map/js/main.js
@@ -10,3 +10,24 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 }).addTo(map)
 
 L.geoJson(statesData).addTo(map)
+
+// Function to determine color to be added to state dependent on density
+function getColor (density) {
+
+    return density > 1000 ? '#000000':
+           density > 500 ?  '#241e20':
+           density > 200 ?  '#44373b':
+           density > 100 ?  '#655157':
+           density > 50 ?   '#896d75':
+           density > 25 ?   '#af8a95':
+           density > 10 ?   '#d6a8b6':
+                            '#ffc7d8'  
+}
+
+console.log(getColor(545))
+
+
+
+
+
+
